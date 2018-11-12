dca9e0dc11b5697249e4f154942b6d918b23314f
laurent <gillet_l@hotmail.com>
2018-11-12 18:57:47 +0100

    Install comments plugin

--- cache/compiled/files/ab150fac77dc4a988be61655d608dc90.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'C:/wamp/www/Naturamat/user/data/licenses.yaml',
-    'modified' => 1540491301,
-    'data' => [
-        
-    ]
-];
